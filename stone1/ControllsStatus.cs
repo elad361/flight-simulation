601f8af7f8eac5914e7211f3d9e5ab4e081f61b1
elad361 <72965673+elad361@users.noreply.github.com>
2021-08-11 11:08:15 +0300

    Add files via upload

---
+++ stone1/ControllsStatus.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Threading;
+
+namespace stone1
+{
+    class ControllsStatus
+    {
+        ViewModel runner;
+        string[] parameters;
+        double pixelsFromLeft;
+        double pixelsFromTop;
+        public string KnobPixelsFromLeft
+        {
+            set
+            {
+                double x = double.Parse(value);
+                pixelsFromLeft = 205 + ((x - AileronMinVal)  / (AileronMaxVal - AileronMinVal))*(90);
+                runner.KnobPixelsFromLeft = KnobPixelsFromLeft;
+            }
+            get
+            {
+                return pixelsFromLeft.ToString();
+            }
+        }
+
+        public string KnobPixelsFromTop
+        {
+            set
+            {
+                double x = double.Parse(value);
+                pixelsFromTop = 105 + ((x - ElevatorMinVal) / ElevatorMaxVal - ElevatorMinVal) * 60;
+                runner.KnobPixelsFromTop = KnobPixelsFromTop;
+            }
+            get
+            {
+                return pixelsFromTop.ToString();
+            }
+        }
+        
+        public double ElevatorMinVal { set; get; }
+        public double ElevatorMaxVal { set; get; }
+        public double AileronMinVal { get; set; }
+        public double AileronMaxVal { get; set; }
+        public ControllsStatus(ViewModel sr, double ailMax, double ailMin, double elMax, double elMin, string[] par)
+        {
+            runner = sr;
+            AileronMinVal = ailMax;
+            AileronMaxVal = ailMin;
+            ElevatorMaxVal = elMax;
+            ElevatorMinVal = elMin;
+            parameters = par;
+            pixelsFromTop = 105;
+            runner.KnobPixelsFromTop = "105";
+            pixelsFromLeft = 245;
+            runner.KnobPixelsFromLeft = "245";
+            runner.RubberVal = "0";
+            runner.ThrottleVal = "0";
+            runner.Altitude = "0";
+            runner.AirSpeed = "0";
+            runner.HeadingDeg = "0";
+            runner.PitchDeg = "0";
+            runner.Yaw = "0";
+        }
+        public void updateControllers(double[] line)
+        {
+            KnobPixelsFromLeft = line[Array.IndexOf(parameters, "aileron")].ToString();
+            KnobPixelsFromTop = line[Array.IndexOf(parameters, "elevator")].ToString();
+            runner.RubberVal = line[Array.IndexOf(parameters, "rudder")].ToString();
+            runner.ThrottleVal = line[Array.IndexOf(parameters, "throttle0")].ToString();
+            runner.Altitude = line[Array.IndexOf(parameters, "altitude-ft")].ToString();
+            runner.AirSpeed = line[Array.IndexOf(parameters, "airspeed-kt")].ToString();
+            runner.HeadingDeg = line[Array.IndexOf(parameters, "heading-deg")].ToString();
+            runner.PitchDeg = line[Array.IndexOf(parameters, "pitch-deg")].ToString();
+            runner.RollDeg = line[Array.IndexOf(parameters, "roll-deg")].ToString();
+            runner.Yaw = line[Array.IndexOf(parameters, "side-slip-deg")].ToString();
+        }
+
+        internal ThreadStart updateControllers()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ stone1/MyClient.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace stone1
+{
+    class MyClient : Client
+    {
+        TcpClient client;
+        NetworkStream stream;
+        private readonly object balanceLock = new object();
+        private readonly object balanceLock2 = new object();
+        public void Connect(string ip, int port)
+        {
+            try
+            {
+                client = new TcpClient(ip, port);
+                // Get a client stream for reading and writing.
+                stream = client.GetStream();
+                stream.ReadTimeout = 10000;
+                // FINISHED CONNECTION.
+            }
+            catch (Exception e)
+            {
+                this.client = null;
+                throw e;
+            }
+        }
+        public bool CheckConnectionStatus()
+        {
+            // return false when connected
+            if (client != null)
+            {
+                return !this.client.Connected;
+            }
+            else
+            {
+                return true;
+            }
+        }
+        public void Disconnect()
+        {
+            try
+            {
+                // Close everything.
+                stream.Close();
+                client.Close();
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+
+        }
+
+        public void Write(string command)
+        {
+
+            if (client != null)
+            {
+                Byte[] data = System.Text.Encoding.ASCII.GetBytes(command);
+
+
+                // Send the message to the connected TcpServer. 
+                stream.Write(data, 0, data.Length);
+            }
+            else
+            {
+                Console.WriteLine("not connected to tcp");
+            }
+
+        }
+
+        public string Read(string value)
+        {
+
+            if (client != null)
+            {
+                Byte[] data = new Byte[256];
+                // String to store the response ASCII representation.
+                String responseData = String.Empty;
+                try
+                {
+                    // Read the first batch of the TcpServer response bytes.
+                    int bytes = stream.Read(data, 0, data.Length);
+                    responseData = System.Text.Encoding.ASCII.GetString(data, 0, bytes);
+                    return responseData;
+                }
+                catch (Exception e)
+                {
+                    if (CheckConnectionStatus())
+                    {
+                        throw e;
+                    }
+                    else
+                    {
+                        //In case we didnt get the request value  from simulator after timeout, then we ignore and preceed forward
+                        return value;
+                    }
+                }
+            }
+            else
+            {
+                Console.WriteLine("not connected to tcp.");
+                return "";
+            }
+        }
+        public void SendArrayAsLine(double[] arr)
+        {
+            string line = "";
+            foreach(double x in arr)
+            {
+                line = line + x.ToString() + ",";
+            }
+            Write(line);
+        }
+    }
+}
